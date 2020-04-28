6ab272bdf47259fa2a3b64b31d142f55149a2b74
Ricar8o <andres.martinez-d@mail.escuelaing.edu.co>
2020-04-27 20:47:04 -0500

    documentacion

--- src/main/java/edu/eci/arsw/treecore/persistence/TreeCoreStore.java
+++ src/main/java/edu/eci/arsw/treecore/persistence/TreeCoreStore.java
@@ -9,35 +9,35 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface TreeCoreStore {
 	/**
-	 * 
-	 * @param ruta
-	 * @return
-	 * @throws TreeCoreStoreException
+	 * Metodo para consultar archivos de una ruta
+	 * @param ruta Ruta de la carpeta a consultar
+	 * @return Lista de los archivos en la carpeta. 
+	 * @throws TreeCoreStoreException Si la ruta no existe.
 	 */
 	public List<String> consultar(final String ruta) throws TreeCoreStoreException;
 
 	/**
-	 * 
-	 * @param localFile
-	 * @param path
-	 * @param option
+	 * Metodo que sube un archivo.
+	 * @param localFile archivo que se subira.
+	 * @param path ruta para subir el archivo
+	 * @param option opcion, si es para publicar o sustituir un archivo.
 	 * @throws TreeCoreStoreException
 	 */
 	public void uploadFile(File localFile, String path, String option) throws TreeCoreStoreException;
 
 	/**
-	 * 
-	 * @param path
-	 * @return
-	 * @throws TreeCoreStoreException
+	 * Metodo para descargar un archivo.
+	 * @param path ruta del archivo.
+	 * @return Nombre y enlace de descarga del archivo.
+	 * @throws TreeCoreStoreException Si la ruta no existe.
 	 */
 	public List<String> downloadFile(String path) throws TreeCoreStoreException;
 
 	/**
-	 * 
-	 * @param file
-	 * @param path
-	 * @param option
+	 * Metodo que recibe parte de un archivo y lo sube.
+	 * @param file archivo obtenido de la solicitud http
+	 * @param path ruta para subir el archivo
+	 * @param option opcion, si es para publicar o sustituir un archivo.
 	 * @throws TreeCoreStoreException
 	 */
 	public void receiveFile(MultipartFile file, String path, String option) throws TreeCoreStoreException;
--- src/main/java/edu/eci/arsw/treecore/services/TreeCoreStoreServices.java
+++ src/main/java/edu/eci/arsw/treecore/services/TreeCoreStoreServices.java
@@ -8,34 +8,34 @@ import edu.eci.arsw.treecore.exceptions.TreeCoreStoreException;
 
 public interface TreeCoreStoreServices {
 	/**
-	 * 
-	 * @param ruta
-	 * @return
-	 * @throws TreeCoreStoreException
+	 * Metodo para consultar archivos de una ruta
+	 * @param ruta Ruta de la carpeta a consultar
+	 * @return Lista de los archivos en la carpeta. 
+	 * @throws TreeCoreStoreException Si la ruta no existe.
 	 */
-	public List<String> consultar(final String ruta) throws TreeCoreStoreException;
+	public List<String> consultar(String ruta) throws TreeCoreStoreException;
 
 	/**
-	 * 
-	 * @param path
-	 * @return
-	 * @throws TreeCoreStoreException
+	 * Metodo para descargar un archivo.
+	 * @param path ruta del archivo.
+	 * @return Nombre y enlace de descarga del archivo.
+	 * @throws TreeCoreStoreException Si la ruta no existe.
 	 */
 	public List<String> downloadFile(String path) throws TreeCoreStoreException;
 
 	/**
-	 * 
-	 * @param file
-	 * @param path
-	 * @param option
+	 * Metodo que recibe parte de un archivo para subirlo.
+	 * @param file archivo obtenido de la solicitud http
+	 * @param path ruta para subir el archivo
+	 * @param option opcion, si es para publicar o sustituir un archivo.
 	 * @throws TreeCoreStoreException
 	 */
 	public void receiveFile(MultipartFile file, String path, String option) throws TreeCoreStoreException;
 
 	/**
-	 * 
-	 * @param tokenId
-	 * @return
+	 * Metodo para obtener el token de acceso de la app.
+	 * @param tokenId id en la base de datos
+	 * @return token de acceso a la carpeta
 	 * @throws TreeCoreStoreException
 	 */
 	public String getAccessToken(int tokenId) throws TreeCoreStoreException;
