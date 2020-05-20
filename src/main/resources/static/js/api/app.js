99907e4bcb5fc51ca934e1df97e7a5d61988bbc1
nduran06 <natalia.duran@mail.escuelaing.edu.co>
2020-05-20 13:27:45 -0500

    mejoras vista

--- src/main/resources/static/js/api/app.js
+++ src/main/resources/static/js/api/app.js
@@ -1,17 +1,19 @@
 var apiclient = apiclient;
-var app = (function () {
+var app = (function() {
 
-	var prueba = function () {
+	var prueba = function() {
 		alert("esto es una prueba")
 	}
 
-	var clearField = function (object) {
+	var clearField = function(object) {
 		object.value = "";
 	}
 
-	var passHash = function (objPassId) {
+	var passHash = function(objPassId) {
 		var pwdObj = document.getElementById(objPassId);
-		var hashObj = new jsSHA("SHA-512", "TEXT", { numRounds: 1 });
+		var hashObj = new jsSHA("SHA-512", "TEXT", {
+			numRounds : 1
+		});
 		hashObj.update(pwdObj.value);
 		var hash = hashObj.getHash("HEX");
 		clearField(pwdObj);
@@ -20,22 +22,21 @@ var app = (function () {
 
 	}
 
-	var loginUser = function (username) {
+	var loginUser = function(username) {
 		clearField(document.getElementById('username'));
 		passHash = passHash('pass');
 
 		var newUser = {
-			correo: username,
-			nombre: "",
-			passwd: passHash,
-			invitaciones: [],
-			notificaciones: []
+			correo : username,
+			nombre : "",
+			passwd : passHash,
+			invitaciones : [],
+			notificaciones : []
 		};
 		apiclient.loginUser(JSON.stringify(newUser), username);
 	}
 
-
-	var validarEmail = function (valor) {
+	var validarEmail = function(valor) {
 		var patron = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
 
 		if (valor.search(patron) == 0) {
@@ -45,9 +46,7 @@ var app = (function () {
 
 	}
 
-
-
-	var addUser = function (username, nombre, passwd, repPasswd) {
+	var addUser = function(username, nombre, passwd, repPasswd) {
 		validEmail = validarEmail(username);
 
 		if (validEmail) {
@@ -57,11 +56,11 @@ var app = (function () {
 				passHash('repPasswd');
 
 				var newUser = {
-					correo: username,
-					nombre: nombre,
-					passwd: passwdHash,
-					invitaciones: [],
-					notificaciones: []
+					correo : username,
+					nombre : nombre,
+					passwd : passwdHash,
+					invitaciones : [],
+					notificaciones : []
 				};
 				apiclient.addUser(JSON.stringify(newUser), manageRegError);
 			}
@@ -76,97 +75,116 @@ var app = (function () {
 		}
 	}
 
-
-	var addProject = function () {
+	var addProject = function() {
 		apiclient.getUser(postProject);
 	}
 
-	var postProject = function (resp) {
-		var newProject = {
-			id: 1,
-			nombre: $("#pnombre").val(),
-			descripcion: $("#pdescripcion").val(),
-			creador: resp,
-			participantes: [resp],
-			mensajes: []
-		};
-		apiclient.addProject(JSON.stringify(newProject));
+	var replaceSpace=function(value){
+		value.replace(/ /g, "");
+	}
+	
+	var postProject = function(resp) {
+
+		nombreP = $("#pnombre").val();
+		replaceSpace(nombreP);
+		descP = $("#pdescripcion").val();
+		replaceSpace(descP);
+
+		if (nombreP == "" || descP == "") {
+			swal({
+				icon : 'error',
+				title : 'Oops...',
+				text : 'Nombre o Descripción no válidos',
+			})
+		}
+
+		else {
+			var newProject = {
+				id : 1,
+				nombre : nombreP,
+				descripcion : descP,
+				creador : resp,
+				participantes : [ resp ],
+				mensajes : []
+			};
+			apiclient.addProject(JSON.stringify(newProject));
+		}
+
 	}
 
-
-	var onload = function () {
+	var onload = function() {
 		apiclient.getUsers(getTable);
 	}
 
-	var verificar = function () {
+	var verificar = function() {
 		if (localStorage.correo != null) {
 			location.replace("/profile.html")
 		}
 	}
 
-	var openNav = function () {
+	var openNav = function() {
 		document.getElementById("mySidenav").style.width = "20%";
 	}
 
-	var closeNav = function () {
+	var closeNav = function() {
 		document.getElementById("mySidenav").style.width = "0";
 	}
 
-	var hiddenElem = function (elemId) {
+	var hiddenElem = function(elemId) {
 		var el = document.getElementById(elemId);
 		el.style.display = (el.style.display == 'none') ? 'block' : 'none';
 	}
 
-	var hiddenErrorLoginMessage = function () {
+	var hiddenErrorLoginMessage = function() {
 		hiddenElem("errorMesLog");
 	}
 
-	var hiddenRegMessage = function () {
+	var hiddenRegMessage = function() {
 		hiddenElem("mesReg");
 	}
 
-	var manageErrorPanel = function (panelDiv, panelPos) {
+	var manageErrorPanel = function(panelDiv, panelPos) {
 		$(panelDiv).dialog({
-			autoOpen: false,
-			hide: "puff",
-			show: "slide",
-			height: 100,
-			modal: true,
-			close: function () {
+			autoOpen : false,
+			hide : "puff",
+			show : "slide",
+			height : 100,
+			modal : true,
+			close : function() {
 				location.reload();
 			},
-			overlay: {
-				opacity: 0.5,
-				background: "black"
+			overlay : {
+				opacity : 0.5,
+				background : "black"
 			}
 		});
 
 		$(panelDiv).dialog({
-			position: {
-				my: 'top',
-				at: 'top',
-				of: $(panelPos)
+			position : {
+				my : 'top',
+				at : 'top',
+				of : $(panelPos)
 			}
 		});
 
 		$(panelDiv).dialog("open");
 	}
 
-	var manageLoginError = function () {
+	var manageLoginError = function() {
 		manageErrorPanel("#errorMesLog", '#errorMesLogPos');
 	}
 
-	var manageRegMailError = function () {
+	var manageRegMailError = function() {
 		changeRegErrorDivText("Invaid Mail");
 		manageErrorPanel("#mesReg", '#errorMesRegPos');
 	}
 
-	var manageRegError = function (ans) {
+	var manageRegError = function(ans) {
 		changeRegErrorDivText(ans["responseText"]);
 		manageErrorPanel("#mesReg", '#errorMesRegPos');
 	}
 
-	var changeRegErrorDivText = function (message) {
+	var changeRegErrorDivText = function(message) {
 		$('#mesReg').html(message);
 	}
 
@@ -189,20 +207,20 @@ var app = (function () {
 			}
 		}
 	}
-	
+
 	return {
-		loginUser: loginUser,
-		addUser: addUser,
-		prueba: prueba,
-		addProject: addProject,
-		verificar: verificar,
-		openNav: openNav,
-		closeNav: closeNav,
-		hiddenErrorLoginMessage: hiddenErrorLoginMessage,
-		hiddenRegMessage: hiddenRegMessage,
-		manageLoginError: manageLoginError,
-		manageRegError: manageRegError,
-		print: print,
-		block:block
+		loginUser : loginUser,
+		addUser : addUser,
+		prueba : prueba,
+		addProject : addProject,
+		verificar : verificar,
+		openNav : openNav,
+		closeNav : closeNav,
+		hiddenErrorLoginMessage : hiddenErrorLoginMessage,
+		hiddenRegMessage : hiddenRegMessage,
+		manageLoginError : manageLoginError,
+		manageRegError : manageRegError,
+		print : print,
+		block : block
 	};
 })();
